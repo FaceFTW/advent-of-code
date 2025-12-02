c91ca6388c582aba9cd0456db833d5030f7ef29d
Alex Westerman <alex@faceftw.dev>
2025-12-01 23:36:20 -0500

    Initial Day 1 part 1, got a math error somewhere

--- 2025/day1/src/main.rs
+++ 2025/day1/src/main.rs
@@ -4,13 +4,14 @@ use std::{
     ops::{Add, Sub},
 };
 
+#[derive(Debug)]
 enum Direction {
     Left,
     Right,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct SafeNumber(u8);
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct SafeNumber(u16);
 
 impl Add for SafeNumber {
     type Output = SafeNumber;
@@ -23,7 +24,7 @@ impl Add for SafeNumber {
         }
 
         if acc + rhs_acc > 100 {
-            SafeNumber(rhs_acc - acc)
+            SafeNumber(acc - (100 - rhs_acc))
         } else {
             SafeNumber(acc + rhs_acc)
         }
@@ -40,19 +41,23 @@ impl Sub for SafeNumber {
             rhs_acc = rhs_acc - 100;
         }
 
-        if acc + rhs_acc > 100 {
-            SafeNumber(rhs_acc - acc)
+        if acc + rhs_acc > 100 || rhs_acc >= acc {
+            SafeNumber(acc + 100 - rhs_acc)
         } else {
             SafeNumber(acc - rhs_acc)
         }
     }
 }
 
-struct Instruction(Direction, u8);
+#[derive(Debug)]
+struct Instruction {
+    dir: Direction,
+    len: SafeNumber,
+}
 
 fn main() {
     let mut string = String::new();
-    match File::open("day/input.txt") {
+    match File::open("2025/day1/input.txt") {
         Ok(mut file) => {
             let _ = file.read_to_string(&mut string);
         }
@@ -61,18 +66,52 @@ fn main() {
 
     let data = parse_data(string.as_str());
 
-    part1();
+    part1(data.as_slice());
     part2();
 }
 
-fn parse_data(string: &str) -> Vec {
-    string.trim_end().split("\n").map(|raw| {}).collect()
+fn parse_data(string: &str) -> Vec<Instruction> {
+    string
+        .trim_end()
+        .split("\n")
+        .map(|raw| {
+            println!("{raw}");
+            let dir = match raw.chars().nth(0) {
+                Some('L') => Direction::Left,
+                Some('R') => Direction::Right,
+                _ => panic!("Malformed instruction-Unexpected Direction"),
+            };
+
+            let num_only = raw.split_at(1).1.trim();
+            println!("{num_only}");
+            let len = SafeNumber(
+                u16::from_str_radix(num_only, 10).expect("Malformed Instruction - bad number!"),
+            );
+            Instruction { dir, len }
+        })
+        .collect()
 }
 
-fn part1() {
-    println!("Day  Part 1 result: {final_result}");
+fn part1(data: &[Instruction]) {
+    let mut acc = 0;
+    let mut safe_state = SafeNumber(50);
+
+    for instr in data {
+        println!("Current State: {}, Instruction: {:?}", safe_state.0, instr);
+        match instr.dir {
+            Direction::Left => safe_state = safe_state - instr.len,
+            Direction::Right => safe_state = safe_state + instr.len,
+        }
+        println!("New State: {}", safe_state.0);
+
+        if safe_state.0 == 0 {
+            acc += 1;
+        }
+    }
+
+    println!("Day  Part 1 result: {acc}");
 }
 
 fn part2() {
-    println!("Day  Part 2 result: {final_result}");
+    // println!("Day  Part 2 result: {final_result}");
 }
